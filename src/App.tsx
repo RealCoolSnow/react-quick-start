b0869e39244311b13695af331cc166427a4f08d1
CoolSnow <coolsnow2020@gmail.com>
2021-04-21 16:25:38 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import logo from './assets/logo.svg'
 
 const App = () => {
+  useEffect(() => {
+    console.log('app created')
+  })
   return (
     <div className='flex justify-center items-center h-screen bg-gray-700'>
       <img src={logo} className='w-40 h-40' alt='logo' />
